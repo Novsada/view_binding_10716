d10396483ff403f203d2a0169dac7035ab1f42ed
Novsada <NovsadaPhasa@gmail.com>
2022-09-15 14:26:55 +0700

    view_binding_10716

---
+++ app/src/main/java/com/example/view_binding_10716/TaskList.kt
@@ -0,0 +1,12 @@
+package com.example.view_binding_10716
+
+object TaskList {
+    val  taskList = listOf<Task> (
+        Task("Chapter 1","Bangun Tidur","Kegiatan Sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo Gacha Mama Yaemiko tercinta/Keqing Wangi"),
+        Task("Chapter 3","Ngopi","Habis Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4","dimana mama Ei?","Buat Adu Mekanik Shnwcase Ayaka Pyro"),
+        Task("Chapter 5","Upload ke Tiktok", "Buat Pamer ke view Showcase Ayaka Pyro C6 + Bennet C6")
+    )
+
+}
